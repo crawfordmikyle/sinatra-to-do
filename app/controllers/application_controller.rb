81a40405eb8bf48b7da77bbb609f068cfa6ca27f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-27 10:09:07 -0600

    Added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < Sinatra::Base
 set :views, Proc.new { File.join(root, "../views/") }
 
-get '/' do
-	erb :index
-end 
+configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+	set :session_secret, "Ruby For Dayz"
+ end
+
+helpers do
+	def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def is_an_email?
+      if params[:email].include?("@") && params[:email].include?(".")
+        true
+      else
+        false
+      end 
+    end 
+  end
 end 
