13a2ebdb506637cdaf34fc731dcba18e096dcc11
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-27 13:02:07 -0600

    Added show user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,13 +13,27 @@ class UserController < ApplicationController
 		end 
 	end 
 
+	get '/login' do 
+		if logged_in?
+			@user = current_user
+			redirect "/users/#{@user.slug}"
+		else 
+			erb :'users/login'
+		end 
+	end 
+
 	post '/users/new' do
 		if valid_user_params? && !user_already_exists?
 			@user = User.create(params)
+			session[:user_id] = @user.id
 			redirect "/users/#{@user.slug}"
 		else
 			flash[:message] = "Invalid Entry"
 			redirect '/signup'
 		end 
+	end 
+
+	get '/users/:slug' do 
+		@user = current_user
 	end 
 end 
